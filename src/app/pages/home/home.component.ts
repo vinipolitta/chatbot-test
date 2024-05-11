6be59576398f11b2dcae1ae6dbf4852f4989c6c4
vinipolitta <vinicius.politta1@gmail.com>
2024-05-11 09:15:47 -0300

    feat: add initial logic to open chat

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { ArrowRightComponent } from '../../icons/arrow-right/arrow-right.component';
 import { SparkleComponent } from '../../icons/sparkle/sparkle.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    ArrowRightComponent, SparkleComponent
-  ],
+  imports: [ArrowRightComponent, SparkleComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor(private router: Router) {}
+  openChat() {
+    this.router.navigate(['/chat']);
+  }
 }
